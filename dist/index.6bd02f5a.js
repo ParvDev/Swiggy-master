7b3a12a3746dff13af7030ac2db20c8d46de2710
Parv VijayVargiya <108187074+ParvDev@users.noreply.github.com>
2023-04-12 19:28:46 +0530

    Add files via upload

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("h1", {
+    id: "title"
+}, "Heading 1");
+const heading = React.createElement("h2", {
+    id: "title"
+}, "Heading 2");
+const container = React.createElement("div", {
+    id: "container"
+}, "Heading 1");
+
+//# sourceMappingURL=index.6bd02f5a.js.map
